a428af0cc4d168e49e0b9c0e1264924930e9eb23
FoxNick <354435383@qq.com>
2024-09-16 12:42:45 +0800

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -40,23 +40,10 @@ if __name__ == '__main__':
     """
     mod_test = '''import { fib } from "./fib_module.js";
     import q from "./1.js";
-    import cheerio from './cheerio.min.js';
-    import './crypto-js.js';
-    import 模板 from './模板.js'
-    import {gbkTool} from './gbk.js'
 
     console.log("Hello World 你好");
 
     console.log(typeof(q.add));
 
     console.log("fib(10)=", fib(10));'''
-    ctx = Context()
-    ctx.moduleloader(moduleloader)
-    #print(ctx.getmoduleloader('1.js'))
-    ctx.add_callable("log", handleLog)
-    ctx.eval("const console = {log};")
-    print("work dir:", os.getcwd())
-    ctx.module(module_js, "11.js")
-    ctx.module('import qq from "./drpy2.js";\nconsole.log(typeof(qq.addd));', "2.js")
-    ctx.eval('console.log(1+1)')
-    ctx.module(mod_test)
+
